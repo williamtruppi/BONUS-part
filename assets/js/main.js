315b1640a1fb3d657ab9d355cfce74ab834008de
William Truppi <williamt@hotmail.it>
2020-12-20 12:47:22 +0100

    aggiungo le task cancellate al cestino

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,10 +14,14 @@ let app = new Vue ({
       console.log(singleTask);
       if (singleTask.length > 4){
         this.taskGiornaliere.push(singleTask);
-      } 
+      } else {
+        alert = "Inserisci una task più lunga di 4 caratteri"
+      }
     },
 
     cancelTask: function(index) {
+      console.log(this.taskGiornaliere[index]);
+      this.eliminatedTask.push(this.taskGiornaliere[index]);
       this.taskGiornaliere.splice(index, 1);
     }
 
